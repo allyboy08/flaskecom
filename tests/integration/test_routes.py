ba0187aeeac265d95b1c5392bcc8005e3e6a3e5d
allyboy08 <alexsassman65@gmail.com>
2021-06-14 16:25:59 +0200

    update

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -2,6 +2,8 @@
 # sys.path.append("tests/test_base")
 from tests.test_base import BaseTest,db
 from market.models import User, Item
+from flask import request
+from flask_login import current_user
 # from market.__init__ import db
 
 
@@ -52,15 +54,43 @@ class TestLogin(BaseTest):
             self.assertIn(b'Username and password are not match! Please try again', response.data)
             
 
-# class TestMarket(BaseTest):
-#     def test_can_purchase(self):
-#         with self.app:
+
             
 
 class TestLogout(BaseTest):
     def test_(self):
         with self.app:
-            pass
+            response = self.app.post('/register', data=dict(
+                username='steve', email_address='okays@gmail.com',
+                password1='python1', password2='python1'
+            ), follow_redirects=True)
+            
+            response = self.app.post('/login', data=dict(
+                username='steve', password='python1'
+            ), follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Success! You are logged in as: steve', response.data)
+            user = db.session.query(User).filter_by(username='steve').first()
+            self.assertTrue(user)
+            #logs user out
+            response = self.app.get('/logout', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'You have been logged out!', response.data)
+            
+    
+    
+            #redirects to logout page
+            self.assertIn('/home', request.url)
+            self.assertFalse(current_user.is_active)
+
+
+# class TestMarket(BaseTest):
+#     def test_can_purchase(self):
+#         with self.app:
+            
+            
+           
+           
 
 
 
