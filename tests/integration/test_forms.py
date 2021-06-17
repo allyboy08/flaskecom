b4416aef555d8a0a2358508538b435ca799c816f
allyboy08 <alexsassman65@gmail.com>
2021-06-17 13:05:01 +0200

    updated

--- tests/integration/test_forms.py
+++ tests/integration/test_forms.py
@@ -1,3 +1,4 @@
+from wtforms.validators import ValidationError
 from tests.test_base import BaseTest, db
 from market.__init__ import db
 from market.models import User
@@ -9,23 +10,26 @@ class TestRegister(BaseTest):
     def test_valid_username(self):
         with self.app:
             # user = User(username='qwerty', email_address='test@gmail.com', password_hash='password')
-            response = self.app.post('/register', data=dict(
+            self.app.post('/register', data=dict(
                 username='ken', email_address='okay1@gmail.com',
                 password1='python1', password2='python1'
             ), follow_redirects=True)
-            user = db.session.query(User).filter_by(username='ken').first()
-            self.assertTrue(user)
-            valid_error = 'Username already exists! Please try a different username'
-            self.assertTrue('Username already exists! Please try a different username', valid_error)
+            class Username():
+                data = 'ken'
+            with self.assertRaises(ValidationError) as context:
+                RegisterForm().validate_username(Username)
+                self.assertEqual('Username already exists! Please try a different username', str(context.exception))
             
     def test_valid_email(self):
         with self.app:
             # user = User(username='qwerty', email_address='test@gmail.com', password_hash='password')
-            response = self.app.post('/register', data=dict(
+            self.app.post('/register', data=dict(
                 username='paul', email_address='okay2@gmail.com',
                 password1='python1', password2='python1'
             ), follow_redirects=True)
-            user = db.session.query(User).filter_by(email_address='okay2@gmail.com').first()
-            self.assertTrue(user)
-            valid_error = 'Email Address already exists! Please try a different email address'
-            self.assertTrue('Email Address already exists! Please try a different email address', valid_error)
+            class Email():
+                data = 'okay2@gmail.com'
+            with self.assertRaises(ValidationError) as context:
+                RegisterForm().validate_email_address(Email)
+                self.assertEqual('Email Address already exists! Please try a different email address', str(context.exception))
+            
